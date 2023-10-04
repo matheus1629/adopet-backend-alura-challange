e758364d49b476d9dc55d86a267e54a270f47265
Matheus Palermo <matheus.palermo@dbccompany.com.br>
2023-10-04 17:11:36 -0300

    home wip

--- root/frontend/src/app/pages/home/home.component.ts
+++ root/frontend/src/app/pages/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PetService } from 'src/app/services/pet.service';
+import { IForm } from 'src/shared/interfaces/form.interface';
+import { IPet } from 'src/shared/interfaces/pet.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  pets!: IPet[];
 
+  constructor(private petService: PetService) {}
+
+  ngOnInit(): void {
+    this.petService.getAllPetsAvailable().subscribe({
+      next: (data) => {
+        this.pets = data;
+        console.log(this.pets);
+      },
+      error: (err) => {
+        console.error('Error: ', err);
+      },
+    });
+  }
 }
---
+++ root/frontend/src/shared/interfaces/pet.interface.ts
@@ -0,0 +1,12 @@
+export interface IPet {
+  id: number;
+  name: string;
+  age: number;
+  size: string;
+  description: string;
+  picture: string;
+  adoptionDate: string;
+  adopted: number;
+  idDonor: number;
+  idAdopter: number;
+}
