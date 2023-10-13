0906fb663a901e873e440e86b550aed722c5a48f
Matheus Palermo <matheus.palermo@dbccompany.com.br>
2023-10-13 09:24:00 -0300

    shared imports

--- root/frontend/src/app/shared/shared.module.ts
+++ root/frontend/src/app/shared/shared.module.ts
@@ -1,27 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
-import { ButtonComponent } from './button/button.component';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { RouterModule } from '@angular/router';
+
+import { HeaderComponent } from './header/header.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [HeaderComponent, ButtonComponent],
   exports: [HeaderComponent, ButtonComponent],
-  imports: [
-    CommonModule,
-    RouterModule,
-    MatMenuModule,
-    MatIconModule,
-    MatRadioModule,
-    MatButtonModule,
-    MatDialogModule,
-    MatProgressSpinnerModule,
-  ],
+  imports: [CommonModule, RouterModule, MatMenuModule, MatIconModule, MatProgressSpinnerModule],
 })
 export class SharedModule {}
